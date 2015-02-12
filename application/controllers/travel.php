73a9ecc5bdc8561b166cb209a17b6138c64fe96b
Oshanz <spiritualmirror@gmail.com>
2015-02-12 16:14:42 +0530

    menu bar ok

--- application/controllers/travel.php
+++ application/controllers/travel.php
@@ -20,6 +20,7 @@ class Travel extends CI_Controller {
 
     function index() {
         $this->load->view('template/header');
+        $this->load->view('template/menu');
         $this->load->view('travel/index');
         $this->load->view('template/footer');
     }
--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -1,8 +1,8 @@
 <nav>
     <div>
         <ul>
-            <li><a href="dashbord"><div class="icono-home"></div><br>Design</a></li>
-            <li><a href="currentPosition"><div class="icono-sitemap"></div><br>View</a></li>
+            <li><a href="design"><div class="icono-image"></div><br>Design</a></li>
+            <li><a href="travel"><div class="icono-sitemap"></div><br>View</a></li>
             <li style="float: right"><a href="login/logout"><div class="icono-power"></div><br>Logout</a></li>
         </ul>
     </div>
