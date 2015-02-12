0f0eb6e80d921d6dbf65b0a8c3f49170a6000d7f
Oshanz <spiritualmirror@gmail.com>
2015-02-12 16:01:53 +0530

    css icono updated

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -1,20 +1,9 @@
 <nav>
     <div>
         <ul>
-            <li><a href="dashbord"><div class="icono-home"></div><br>Dashboard</a></li>
-            <li><a href="currentPosition"><div class="icono-eye"></div><br>Live Map</a></li>
-            <li><a href="vehiclePath"><div class="icono-pin"></div><br>Fleet Map</a></li>
-            <li><a href="#"><div class="icono-barChart"></div><br>Reports</a>
-                <ul>
-                    <li><a href="distance">Distance</a></li>
-                    <li><a href="speed">Speed</a></li>
-                    <li><a href="parking">Stationary</a></li>                    
-                    <li><a href="fuel">Fuel</a></li>
-                    <li><a href="tempreature">Tempreature</a></li>
-                </ul>
-            </li>
-            <li><a href="#"><div class="icono-gear"></div><br>Admin</a></li>                 
-            <li><a href="login/logout"><div class="icono-power"></div><br>Logout</a></li>
+            <li><a href="dashbord"><div class="icono-home"></div><br>Design</a></li>
+            <li><a href="currentPosition"><div class="icono-sitemap"></div><br>View</a></li>
+            <li style="float: right"><a href="login/logout"><div class="icono-power"></div><br>Logout</a></li>
         </ul>
     </div>
 </nav>
