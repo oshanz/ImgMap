6418b50d42d54365c9c8ec4d76db1236a7e8ecec
Oshanz <spiritualmirror@gmail.com>
2015-02-19 13:02:47 +0530

    making image map

--- application/views/design/form/form_view.js
+++ application/views/design/form/form_view.js
@@ -12,13 +12,17 @@ define(function(require) {
             var reader = new FileReader();
             var self = this;
             reader.onload = function(e) {
-                self.$('#preview').attr('src', e.target.result);//.width(150).height(200)
+                var p = self.$('#preview');
+                p.attr('src', e.target.result);//.width(150).height(200)
+                console.log(p.prop('width'));
+                console.log(p.get(0).naturalWidth);
             };
             this.fileReader = reader;
         },
         render: function() {
             this.$el.html(this.template());
             this.addSubCatRow();
+            this.cancon = this.$('canvas')[0].getContext("2d");
             return this.el;
         },
         events: {
@@ -28,8 +32,13 @@ define(function(require) {
         loadImg: function(e) {
             var ele = $(e.currentTarget)[0];
             if (ele.files && ele.files[0]) {
+                if (!ele.files[0].type.match(/image.*/)) {
+                    alert("Isn't a Image");
+                    return;
+                }
                 this.fileReader.readAsDataURL(ele.files[0]);
-//                this.$('#preview').attr('src', window.URL.createObjectURL(ele.files[0]));
+//                var winURL = window.webkitURL || window.URL;
+//                this.$('#preview').attr('src', winURL.createObjectURL(ele.files[0]));
             }
         },
         addSubCatRow: function() {
