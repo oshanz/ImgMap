fd8c06cf660176e83f4be3bd247b8ec424c13d99
Oshanz <spiritualmirror@gmail.com>
2015-02-24 17:49:44 +0530

    validation added

--- application/views/design/form/img/ozImg_view.js
+++ application/views/design/form/img/ozImg_view.js
@@ -1,70 +1,77 @@
 "use strict";
 
 define(function(require) {
-    var _ = require('underscore'),
-            Backbone = require('backbone'),
-            tpl = _.template(require('text!form/img/ozImg_tpl.html'));
+	var _ = require('underscore'), Backbone = require('backbone'), tpl = _.template(require('text!form/img/ozImg_tpl.html'));
 
-    return Backbone.View.extend({
-        template: tpl,
-        coords: [],
-        render: function() {
-            this.$el.html(this.template());
-            this.ctx = this.$('canvas')[0].getContext("2d");
-            return this.el;
-        },
-        initialize: function() {
-            this.listenTo(this.model, 'change:src', this.resetImg);
-        },
-        resetImg: function() {
-            this.coords = [];
-            var img = this.$('img').get(0), cvs = this.$('canvas');
-            this.ctx.clearRect(0, 0, cvs.prop('width'), cvs.prop('height'));
-            img.src = this.model.get('src');
-            img.style.display = 'none';
-            var w = img.naturalWidth, //naturalWidth,
-                    h = img.naturalHeight;
-            cvs.attr('width', w + 'px');
-            cvs.attr('height', h + 'px');
-            cvs.css('border', '1px solid');
-            this.ctx.drawImage(img, 0, 0, w, h);
-
-        },
-        events: {
-            'click canvas': 'markPoint'
-        },
-        markPoint: function(e) {
-            this.coordvalidate();
-            var crd = JSON.parse(JSON.stringify(this.coords));
-            this.resetImg();
-            this.coords = crd;
-            var img = $(e.currentTarget).offset();
-            var y = e.pageY - img.top;
-            var x = e.pageX - img.left;
-            this.coords.push({
-                X: x,
-                Y: y
-            });
-            this.coords.push(this.coords[0]);
-            //draw path
-            this.ctx.beginPath();
-            this.ctx.moveTo(this.coords[0].X, this.coords[0].Y);
-            for (var i = 1; i < this.coords.length; i++) {
-                this.ctx.lineTo(this.coords[i].X, this.coords[i].Y);
-            }
-            this.ctx.closePath();
-            this.ctx.strokeStyle = "blue";
-            this.ctx.stroke();
-            this.ctx.fillStyle = 'rgba(181,229,255,0.5)';
-            this.ctx.fill();
-            var i = this.model.get('current_i');
-            this.coords.pop();
-            this.model.set('coords_' + i, JSON.parse(JSON.stringify(this.coords)));
-        },
-        coordvalidate: function() {
-
-        }
-    });
+	return Backbone.View.extend({
+		template : tpl,
+		coords : [],
+		render : function() {
+			this.$el.html(this.template());
+			this.ctx = this.$('canvas')[0].getContext("2d");
+			return this.el;
+		},
+		initialize : function() {
+			this.listenTo(this.model, 'change:src', this.resetImg);
+		},
+		resetImg : function() {
+			this.coords = [];
+			var img = this.$('img').get(0), cvs = this.$('canvas');
+			this.ctx.clearRect(0, 0, cvs.prop('width'), cvs.prop('height'));
+			img.src = this.model.get('src');
+			img.style.display = 'none';
+			var w = img.naturalWidth, //naturalWidth,
+			h = img.naturalHeight;
+			cvs.attr('width', w + 'px');
+			cvs.attr('height', h + 'px');
+			cvs.css('border', '1px solid');
+			this.ctx.drawImage(img, 0, 0, w, h);
+		},
+		events : {
+			'click canvas' : 'markPoint'
+		},
+		markPoint : function(e) {
+			var vali = this.coordvalidate();
+			if (!vali) {
+				alert('Too Much Marks');
+				this.resetImg();
+			} else {
+				var crd = JSON.parse(JSON.stringify(this.coords));
+				this.resetImg();
+				this.coords = crd;
+				var img = $(e.currentTarget).offset();
+				var y = e.pageY - img.top;
+				var x = e.pageX - img.left;
+				this.coords.push({
+					X : x,
+					Y : y
+				});
+				this.coords.push(this.coords[0]);
+				//draw path
+				this.ctx.beginPath();
+				this.ctx.moveTo(this.coords[0].X, this.coords[0].Y);
+				for (var i = 1; i < this.coords.length; i++) {
+					this.ctx.lineTo(this.coords[i].X, this.coords[i].Y);
+				}
+				this.ctx.closePath();
+				this.ctx.strokeStyle = "blue";
+				this.ctx.stroke();
+				this.ctx.fillStyle = 'rgba(181,229,255,0.5)';
+				this.ctx.fill();
+				this.coords.pop();
+			}
+			var i = this.model.get('current_i');
+			this.model.set('coords_' + i, JSON.parse(JSON.stringify(this.coords)));
+		},
+		coordvalidate : function() {
+			var i = this.model.get('current_i'), c = this.model.get('coords_' + i);
+			var st = _.map(c, function(o) {
+				return o.X + ',' + o.Y;
+			});
+			st = st.join(',').length;
+			return (st < 240) ? true : false;
+		}
+	});
 });
 
 /**
